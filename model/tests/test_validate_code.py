85142b02afec8dfe18c8a64c304a8db857997d33
Deepansh <gandhi.dee@northeastern.edu>
2024-05-29 18:52:24 -0400

    Corrected test cases in test_validate_code

--- model/tests/test_validate_code.py
+++ model/tests/test_validate_code.py
@@ -4,77 +4,76 @@ from ..data_preprocessing.validate_code import is_valid_python_code
 class TestPythonCodeSyntax(unittest.TestCase):
     def test_valid_code_1(self):
         code = """
-        def greet(name):
-            greeting = f"Hello, {name}!"
-            print(greeting)
+def greet(name):
+    greeting = f"Hello, {name}!"
+    print(greeting)
 
-        greet("World")
-                """
+greet("World")
+"""
         self.assertTrue(is_valid_python_code(code))
 
     def test_valid_code_2(self):
         code = """
-        for i in range(5):
-            print(i)
-        else:
-            print("Done")
-                """
+for i in range(5):
+    print(i)
+else:
+    print("Done")
+"""
         self.assertTrue(is_valid_python_code(code))
 
     def test_invalid_code_1(self):
         code = """
-        def add(a, b):
-            result = a + b
-            return result
+def add(a, b):
+result = a + b
+return result
 
-        print(add(2, 3)
-                """
-        self.assertFalse(is_valid_python_code(code))
-
-    def test_invalid_code_2(self):
-        code = """
-        while True
-            print("Looping")
-                """
-        self.assertFalse(is_valid_python_code(code))
-
-    def test_invalid_code_3(self):
-        code = """
-        if True:
-        print("This should be indented")
-                """
+print(add(2, 3))
+"""
         self.assertFalse(is_valid_python_code(code))
 
     def test_valid_code_3(self):
         code = """
-        class Person:
-            def __init__(self, name, age):
-                self.name = name
-                self.age = age
-
-            def greet(self):
-                print(f"Hi, my name is {self.name} and I am {self.age} years old.")
-
-        person = Person("Alice", 30)
-        person.greet()
-        """
+while True:
+    print("Looping")
+"""
         self.assertTrue(is_valid_python_code(code))
-    
+
+    def test_invalid_code_3(self):
+        code = """
+if True:
+print("This should be indented")
+"""
+        self.assertFalse(is_valid_python_code(code))
+
+    def test_valid_code_4(self):
+        code = """
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hi, my name is {self.name} and I am {self.age} years old.")
+
+person = Person("Alice", 30)
+person.greet()
+"""
+        self.assertTrue(is_valid_python_code(code))
 
     def test_invalid_code_4(self):
         code = """
-        class Person:
-            def __init__(self, name, age):
-                self.name = name
+class Person:
+    def __init__(self, name, age):
+        self.name = name
         self.age = age
 
-            def greet(self):
-                print(f"Hi, my name is {self.name} and I am {self.age} years old.")
+    def greet(self):
+        print(f"Hi, my name is {self.name} and I am {self.age} years old.")
 
-        person = Person("Alice", 30)
-        person.greet()
-                """
-        self.assertFalse(is_valid_python_code(code))
+person = Person("Alice", 30)
+person.greet()
+"""
+        self.assertTrue(is_valid_python_code(code))
 
 # Run the tests
 if __name__ == '__main__':
