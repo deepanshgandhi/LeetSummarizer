ce2f731f9f2b1209e730a32b8385f36743327747
sanket <shahsanket2107@gmail.com>
2024-06-24 17:35:53 -0400

    MR resolved

---
+++ dags/model_pipeline.py
@@ -0,0 +1,57 @@
+# from airflow import DAG
+# from airflow.operators.python_operator import PythonOperator
+# from datetime import datetime
+# from src.model.load_model import load_model_tokeniser
+# from src.model.save_model import save_model
+# from src.model.model_eval import evaluate_model
+# from src.model.model_training import prepare_dataset,train_model
+
+# dag = DAG(
+#     'model_pipeline',
+#     default_args={
+#         'owner': 'airflow',
+#         'start_date': datetime(2024, 5, 8),
+#     },
+#     schedule_interval=None,
+# )
+
+# # Define tasks
+# task_load_model_tokenizer = PythonOperator(
+#     task_id='load_model_tokenizer',
+#     python_callable=load_model_tokeniser,
+#     provide_context=True,
+#     dag=dag,
+# )
+
+# task_prepare_dataset = PythonOperator(
+#     task_id='prepare_dataset',
+#     python_callable=prepare_dataset,
+#     provide_context=True,
+#     # input from data pipeline
+#     op_kwargs={'input_data': input_data},  # Pass the input data here
+#     dag=dag,
+# )
+
+# task_train_model = PythonOperator(
+#     task_id='train_model',
+#     python_callable=train_model,
+#     provide_context=True,
+#     dag=dag,
+# )
+
+# task_evaluate_model = PythonOperator(
+#     task_id='evaluate_model',
+#     python_callable=evaluate_model,
+#     provide_context=True,
+#     dag=dag,
+# )
+
+# task_save_model = PythonOperator(
+#     task_id='save_model',
+#     python_callable=save_model,
+#     provide_context=True,
+#     dag=dag,
+# )
+
+# # Set task dependencies
+# task_load_model_tokenizer >> task_prepare_dataset >> task_train_model >> task_evaluate_model >> task_save_model
--- dags/src/data_preprocessing/print_final_data.py
+++ dags/src/data_preprocessing/print_final_data.py
@@ -1,4 +1,7 @@
-def print_final_data(data: list) -> None:
+import json
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+
+def print_final_data(data: list, bucket_name: str, destination_blob_name: str, **kwargs) -> None:
     """
     Prints the final cleaned data
 
@@ -8,6 +11,7 @@ def print_final_data(data: list) -> None:
     Returns:
     None
     """
+    print("MAKING CHANGES HERE")
     for item in data:
         code = item.get('Code', '')
         question = item.get('Question','')
@@ -23,5 +27,31 @@ def print_final_data(data: list) -> None:
         print("-"*100)
         print()
         print("-"*100)
+    # Save to a local file
+    local_path = '/tmp/preprocessed_data.json'
+    with open(local_path, 'w') as f:
+        json.dump(data, f)
+    
+    print("Uploading to GCS using GCSHook")
+    upload_to_gcs_using_hook(local_path, bucket_name, destination_blob_name)
 
+def upload_to_gcs_using_hook(local_path: str, bucket_name: str, destination_blob_name: str) -> None:
+    """
+    Uploads a file to a GCS bucket using GCSHook.
+
+    Args:
+    local_path (str): Path to the local file.
+    bucket_name (str): Name of the GCS bucket.
+    destination_blob_name (str): Destination path in the GCS bucket.
+
+    Returns:
+    None
+    """
+    # Initialize the GCSHook
+    gcs_hook = GCSHook()
+    
+    # Upload the file to GCS
+    gcs_hook.upload(bucket_name, destination_blob_name, local_path)
+    
+    print(f"File {local_path} uploaded to {destination_blob_name} in bucket {bucket_name}.")
 
